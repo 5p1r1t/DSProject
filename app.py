3aeee958145605f494aae9883543f8def339b9a6
Sergei Tsoi <to5p1r1t@yandex.ru>
2023-01-30 16:16:47 +1000

    2.0 Добавлен расчет стоимости во всех МО и окраска карты соответствующим образом

--- app.py
+++ app.py
@@ -3,8 +3,9 @@
 # Загрузим необходимые пакеты
 import pandas as pd
 import plotly.express as px
+from datetime import datetime
 #from jupyter_dash import JupyterDash
-from dash import Dash, dcc, html, Input, Output, State, no_update
+from dash import Dash, dcc, html, Input, Output, State, no_update, callback_context
 from dash.exceptions import PreventUpdate
 from catboost import CatBoostRegressor
 import json
@@ -29,7 +30,7 @@ df = pd.read_csv(path+'mo_info.csv')
 # Загрузка модели
 cat_load = CatBoostRegressor()
 CatBoostRegressor.load_model(cat_load, path+'cb_top12')
-
+print (datetime.now().time(), 'Данные и модель загружены.')
 # Конструктор приложения, указываем тему dbc
 #app = JupyterDash(__name__, external_stylesheets=[dbc.themes.MATERIA])
 app = Dash(__name__, external_stylesheets=[dbc.themes.MATERIA, dbc.icons.BOOTSTRAP])
@@ -142,7 +143,7 @@ num_room = html.Div(
         ),
         dcc.Slider(
             id='num_room_sl',
-            min=0,
+            min=1,
             max=20,
             marks=None,
             step=1,
@@ -373,155 +374,126 @@ app.layout = dbc.Container(
 ############################################ Синхронизация input и slider ##############################################
 
 @app.callback(
-    Output('full_sq_sl', 'value'), Input('full_sq', 'value'), State('full_sq_sl', 'value'))
-def update_full_sq_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("full_sq", "value"),
+    Output("full_sq_sl", "value"),
+    Input("full_sq", "value"),
+    Input("full_sq_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "full_sq" else slider_value
+    return value, value
 
 @app.callback(
-    Output('full_sq', 'value'), Input('full_sq_sl', 'value'), State('full_sq', 'value'))
-def update_full_sq(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("life_sq", "value"),
+    Output("life_sq_sl", "value"),
+    Input("life_sq", "value"),
+    Input("life_sq_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "life_sq" else slider_value
+    return value, value
 
 @app.callback(
-    Output('life_sq_sl', 'value'), Input('life_sq', 'value'), State('life_sq_sl', 'value'))
-def update_life_sq_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("kitch_sq", "value"),
+    Output("kitch_sq_sl", "value"),
+    Input("kitch_sq", "value"),
+    Input("kitch_sq_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "kitch_sq" else slider_value
+    return value, value
 
 @app.callback(
-    Output('life_sq', 'value'), Input('life_sq_sl', 'value'), State('life_sq', 'value'))
-def update_life_sq(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("num_room", "value"),
+    Output("num_room_sl", "value"),
+    Input("num_room", "value"),
+    Input("num_room_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "num_room" else slider_value
+    return value, value
+
 
 @app.callback(
-    Output('kitch_sq_sl', 'value'), Input('kitch_sq', 'value'), State('kitch_sq_sl', 'value'))
-def update_kitch_sq_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("floor", "value"),
+    Output("floor_sl", "value"),
+    Input("floor", "value"),
+    Input("floor_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "floor" else slider_value
+    return value, value
 
 @app.callback(
-    Output('kitch_sq', 'value'), Input('kitch_sq_sl', 'value'), State('kitch_sq', 'value'))
-def update_kitch_sq(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("max_floor", "value"),
+    Output("max_floor_sl", "value"),
+    Input("max_floor", "value"),
+    Input("max_floor_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "max_floor" else slider_value
+    return value, value
+
 
 @app.callback(
-    Output('num_room_sl', 'value'), Input('num_room', 'value'), State('num_room_sl', 'value'))
-def update_num_room_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("build_year", "value"),
+    Output("build_year_sl", "value"),
+    Input("build_year", "value"),
+    Input("build_year_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "build_year" else slider_value
+    return value, value
 
 @app.callback(
-    Output('num_room', 'value'), Input('num_room_sl', 'value'), State('num_room', 'value'))
-def update_num_room(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("green_zone_km", "value"),
+    Output("green_zone_km_sl", "value"),
+    Input("green_zone_km", "value"),
+    Input("green_zone_km_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "green_zone_km" else slider_value
+    return value, value
 
 @app.callback(
-    Output('floor_sl', 'value'), Input('floor', 'value'), State('floor_sl', 'value'))
-def update_floor_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
+    Output("ttk_km", "value"),
+    Output("ttk_km_sl", "value"),
+    Input("ttk_km", "value"),
+    Input("ttk_km_sl", "value"),
+)
+def callback(input_value, slider_value):
+    ctx = callback_context
+    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+    value = input_value if trigger_id == "ttk_km" else slider_value
+    return value, value
 
-@app.callback(
-    Output('floor', 'value'), Input('floor_sl', 'value'), State('floor', 'value'))
-def update_floor(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('max_floor_sl', 'value'), Input('max_floor', 'value'), State('max_floor_sl', 'value'))
-def update_max_floor_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('max_floor', 'value'), Input('max_floor_sl', 'value'), State('max_floor', 'value'))
-def update_max_floor(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('build_year_sl', 'value'), Input('build_year', 'value'), State('build_year_sl', 'value'))
-def update_build_year_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('build_year', 'value'), Input('build_year_sl', 'value'), State('build_year', 'value'))
-def update_build_year(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('green_zone_km_sl', 'value'), Input('green_zone_km', 'value'), State('green_zone_km_sl', 'value'))
-def update_green_zone_km_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('green_zone_km', 'value'), Input('green_zone_km_sl', 'value'), State('green_zone_km', 'value'))
-def update_green_zone_km(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('ttk_km_sl', 'value'), Input('ttk_km', 'value'), State('ttk_km_sl', 'value'))
-def update_ttk_km_sl(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
-
-@app.callback(
-    Output('ttk_km', 'value'), Input('ttk_km_sl', 'value'), State('ttk_km', 'value'))
-def update_ttk_km(v_in, v_st):
-    if float(v_in) == float(v_st):
-        raise PreventUpdate
-    else:
-        return float(v_in)
 
 ############################################## Расчет стоимости квартиры ###############################################
-
+# При изменении элементов управления:
+# 1. рассчитывается прогноз для всех округов
+# 2. выводится прогноз для выбранного округа
+# 3. Обновляется карта с нанесением карты цветов в соответствии с прогнозными ценами
 @app.callback(
     [
         Output('price', 'children'),
-        Output('price', 'color')
+        Output('price', 'color'),
+        Output("map", "figure")
     ],
     Input('sub_area', 'value'),
     Input('full_sq', 'value'),
@@ -538,6 +510,10 @@ def update_ttk_km(v_in, v_st):
 )
 def predict(sub_area, full_sq, life_sq, kitch_sq, num_room, floor, max_floor,
             build_year, state, product_type, green_zone_km, ttk_km):
+    print(datetime.now().time(), callback_context.triggered_id, 'callback:')
+    print([sub_area, full_sq, life_sq, kitch_sq, num_room, floor, max_floor,
+    build_year, state, product_type, green_zone_km, ttk_km])
+
     full_sq = float(full_sq)
     life_sq = float(life_sq)
     kitch_sq = float(kitch_sq)
@@ -550,16 +526,15 @@ def predict(sub_area, full_sq, life_sq, kitch_sq, num_room, floor, max_floor,
     ttk_km = float(ttk_km)
 
     if (full_sq < life_sq):
-        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Общая площадь меньше жилой"], "danger"
+        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Общая площадь меньше жилой"], "danger", no_update
     elif (full_sq < kitch_sq):
-        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Общая площадь меньше площади кухни"], "danger"
+        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Общая площадь меньше площади кухни"], "danger", no_update
     elif (full_sq < life_sq + kitch_sq):
-        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Общая площадь меньше суммы жилой площади и кухни"], "danger"
+        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Общая площадь меньше суммы жилой площади и кухни"], "danger", no_update
     elif (max_floor < floor):
-        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Этаж больше кол-ва этажей в здании"], "danger"
+        return [html.I(className="bi bi-x-octagon-fill me-2"),"Ошибка! Этаж больше кол-ва этажей в здании"], "danger", no_update
     else:
-        # Вариант 1: Просто считаем прогноз для набора параметров и возвращаем его
-        data = {
+        data = [{
             'full_sq': full_sq,
             'num_room': num_room,
             'build_year': build_year,
@@ -568,71 +543,68 @@ def predict(sub_area, full_sq, life_sq, kitch_sq, num_room, floor, max_floor,
             'max_floor': max_floor,
             'life_sq': life_sq,
             'product_type': product_type,
-            'sub_area': sub_area,
+            'sub_area': i,
             'ttk_km': ttk_km,
             'green_zone_km': green_zone_km,
             'kitch_sq': kitch_sq
-        }
-        price_pred = cat_load.predict(pd.DataFrame(data, [0]))
-        return [html.I(className="bi bi-check-circle-fill me-2"), '{0:,.2f}'.format(price_pred[0]).replace(',', ' ')], "success"
+        } for i in df['MO']]
+        data = pd.DataFrame(data)
+        price_pred = cat_load.predict(data) # Посчитали прогноз для всех МО
 
+        print(datetime.now().time(), 'Прогноз для всех МО рассчитан.')
 
+        price_pred = pd.concat([data['sub_area'], pd.Series(price_pred).round(2)], axis=1)
+        price_pred.rename(columns={'sub_area': 'MO', 0: 'price_pred'}, inplace=True)
+        df_price_pred = df.merge(price_pred, on='MO', how = 'left')
+
+        # Обновили карту
+        fig = px.choropleth_mapbox(
+            df_price_pred,
+            geojson=mo,
+            color='price_pred',
+            locations='MO_ru',
+            featureidkey='properties.NAME',
+            opacity=0.6,
+            range_color=[df_price_pred['price_pred'].min(), df_price_pred['price_pred'].max()],
+            hover_data=df_price_pred.columns[2:],
+            mapbox_style="open-street-map",
+            center={"lat": 55.59, "lon": 37.5},
+            zoom=7.89,
+            color_continuous_scale=px.colors.diverging.Portland,
+            # animation_frame='area_m',
+            labels={
+                'MO_ru': 'Муниципальное образование ',
+                'area_m': 'Площадь района, км2 ',
+                'raion_popul': 'Население района, чел ',
+                'density_popul': 'Плотность населения, чел/км2 ',
+                'green_zone_part': '% озеленения ',
+                'indust_part': '% промзоны ',
+                'price_pred': 'Прогноз цены, руб '
+            }
+        )
+
+        fig.update_geos(fitbounds="locations", visible=False)
+        fig.update_layout(
+            # mapbox_zoom=20.0,
+            margin={"r": 0, "t": 0, "l": 0, "b": 0}
+        )
+        x = price_pred['price_pred'][price_pred['MO'] == sub_area].iloc[0]
+        print(datetime.now().time(), 'Прогноз {} рассчитан, карта сформирована.'.format(x))
+
+        return [html.I(className="bi bi-check-circle-fill me-2"),'{0:,.2f}'.format(x).replace(',', ' ')], "success", fig
+
+#При клике на карте МО он устанавливается в sub_area
 @app.callback(
     Output("sub_area", "value"),
     Input("map", "clickData"))
 def update_sub_area(clickData):
-    #print (clickData)
+    print (clickData)
     if clickData == None:
-        return df['MO'][0]
+        return no_update
     else:
         return df.loc[df['MO_ru'] == clickData['points'][0]['location'], 'MO'].iloc[0]
 
 
-
-
-@app.callback(
-    Output("map", "figure"),
-    Input("sub_area", "value"))
-def display_choropleth(sub_area):
-    #https://plotly.com/python/choropleth-maps/
-    fig = px.choropleth_mapbox(df,
-                        #title = 'Муниципальные районы',
-                        geojson=mo,
-                        color='density_popul',
-                        locations='MO_ru',
-                        featureidkey='properties.NAME',
-                        #projection="mercator",
-                        opacity=0.3,
-
-                        range_color=[0, df['density_popul'].max()],
-                        hover_data=df.columns[2:],
-                        mapbox_style="open-street-map",
-
-                        #mapbox_style="carto-positron",
-                        center={"lat": 55.59, "lon": 37.5},
-                        zoom=7.89,
-                        #animation_frame='area_m',
-                        labels={'MO_ru': 'Муниципальное образование ',
-                                'area_m': 'Площадь района, км2 ',
-                                'raion_popul': 'Население района, чел ',
-                                'density_popul': 'Плотность населения, чел/км2 ',
-                                'green_zone_part': '% озеленения ',
-                                'indust_part': '% промзоны '
-                                }
-    )
-
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(
-        #mapbox_zoom=20.0,
-        margin={"r": 0, "t": 0, "l": 0, "b": 0}
-    )
-
-
-
-    return fig
-
-
-# Run app and display result inline in the notebook
 #app.run_server(mode='inline')
 if __name__ == '__main__':
     app.run_server(debug=True)
